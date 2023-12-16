6b8c6d4a06202f63e14d8c7a6918def7323f45e6
cloudxqz <114294229+cloudxqz@users.noreply.github.com>
2023-12-16 13:07:05 +0400

    Update title.py

--- title.py
+++ title.py
@@ -1,8 +1,7 @@
 def title(input:str):
     sentense = input.split()  
-    first_char_upper = [word.lower().capitalize() for word in sentense] 
-    result = ' '.join(first_char_upper) 
-    return result
+    first_char_upper = [word[0].upper() + word[1:] for word in sentense]
+    return ' '.join(first_char_upper)
 
 input_string = "F a cA fa Ac"
 result = title(input_string)
